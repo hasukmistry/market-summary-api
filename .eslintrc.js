e415ec3a64b0f0f771d80c2984d80401150aa565
Hasmukh Mistry <hasmukhmistry137@gmail.com>
2021-11-28 16:53:40 +0530

    Adds eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  parser: "@babel/eslint-parser",
+  rules: {
+    "indent": ["error", 4],
+    "comma-dangle": ["error", "never"],
+    "strict": "off",
+    "no-restricted-imports": "off",
+    "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
+    "prefer-arrow-callback": "off",
+    "arrow-body-style": "off",
+    "prefer-const": "off",
+    "prefer-destructuring": "off",
+    "no-console": "off",    
+    "func-names": "off",
+    "import/first": "off",
+    "import/order": "off",
+    "no-unused-vars": "off",
+    "import/newline-after-import": "off",
+    "camelcase": "off",
+    "no-trailing-spaces": ["error", 
+      { 
+        "skipBlankLines": true,
+        "ignoreComments": true
+      }
+    ]
+  },
+};
